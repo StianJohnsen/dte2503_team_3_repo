ac27d90ef1bcd2d5aca5b4acda7f219b7232a7d4
StianJohnsen <stianjohnzz1@gmail.com>
2023-09-27 20:43:44 +0100

    SensorFragment shows both unfiltered and filtered sensor data. Orientation is also showing. Lack the saving feature

--- app/src/main/java/com/example/dashcarr/presentation/tabs/settings/SensorFragment.kt
+++ app/src/main/java/com/example/dashcarr/presentation/tabs/settings/SensorFragment.kt
@@ -7,10 +7,7 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.example.dashcarr.R
 import com.example.dashcarr.databinding.FragmentSensorBinding
 import com.example.dashcarr.presentation.core.BaseFragment
@@ -24,6 +21,27 @@ class SensorFragment : BaseFragment<FragmentSensorBinding>(
     private var accelSensor: Sensor? = null
     private var gyroSensor: Sensor? = null
     private var magnetoSensor: Sensor? = null
+
+    // Accelerometer
+    val rawAccData = FloatArray(3)
+    val rawAccDataIndex = 0
+    val filtAccData = FloatArray(3)
+    val filtAccPrevData = FloatArray(3)
+    var count = 0
+    val beginTime = System.nanoTime()
+    val rc = 0.002f
+
+    // Gyroscope
+    val rawGyroData = FloatArray(3)
+    val rawGyroDataIndex = 0
+    val filtGyroData = FloatArray(3)
+    val filtGyroPrevData = FloatArray(3)
+
+    // Orientation
+    private val accelerometerRading = FloatArray(3)
+    private val magnetometerReading = FloatArray(3)
+    private val rotationMatrix = FloatArray(9)
+    private val orientationAngles = FloatArray(3)
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -44,11 +62,22 @@ class SensorFragment : BaseFragment<FragmentSensorBinding>(
             gyroSensor,
             SensorManager.SENSOR_DELAY_FASTEST
         )
-        sensorManager.registerListener(
-            this,
-            magnetoSensor,
-            SensorManager.SENSOR_DELAY_FASTEST
-        )
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also { accelerometer ->
+            sensorManager.registerListener(
+                this,
+                accelerometer,
+                SensorManager.SENSOR_DELAY_NORMAL,
+                SensorManager.SENSOR_DELAY_UI
+            )
+        }
+        sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)?.also { magneticField ->
+            sensorManager.registerListener(
+                this,
+                magneticField,
+                SensorManager.SENSOR_DELAY_NORMAL,
+                SensorManager.SENSOR_DELAY_UI
+            )
+        }
     }
 
 
@@ -65,23 +94,135 @@ class SensorFragment : BaseFragment<FragmentSensorBinding>(
             sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)!!
 
 
-
     }
 
 
     override fun onSensorChanged(event: SensorEvent?) {
-        if (event?.values != null){
-        if (event?.sensor == accelSensor) {
-                binding.accelUnfiltered.text = getString(R.string.accelerometer_unfiltered_template,event.values[0] + event.values[1] + event.values[2])
-            }
-            if (event?.sensor == gyroSensor){
-                binding.gyroUnfiltered.text = getString(R.string.gyroscope_unfiltered_template,event.values[0] + event.values[1] + event.values[2])
+        if (event?.values != null) {
+            if (event?.sensor == accelSensor) {
+                // Unfiltered Accelerometer
+                binding.accelUnfiltered.text = getString(
+                    R.string.accelerometer_unfiltered_template,
+                    (event.values[0] +
+                            event.values[1]
+                            + event.values[2]).toDouble()
 
+                )
+                // Filtered Accelerometer
+                readAccSensorData(event)
             }
+            // Unfiltered Gyroscope
+            if (event?.sensor == gyroSensor) {
+                binding.gyroUnfiltered.text = getString(
+                    R.string.gyroscope_unfiltered_template,
+
+                    (event.values[0] +
+                            event.values[1]
+                            + event.values[2]).toDouble()
+
+
+                )
+                // Filtered Gyroscope
+                readGyroSensorData(event)
+            }
+            // Orientation
+            if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+                System.arraycopy(
+                    event.values,
+                    0,
+                    accelerometerRading,
+                    0,
+                    accelerometerRading.size
+                )
+            } else if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
+                System.arraycopy(event.values, 0, magnetometerReading, 0, magnetometerReading.size)
+            }
+            updateOrientationAngles()
+
+            binding.orientationText.text = getString(
+                R.string.orientation_tenplate,
+                (orientationAngles[0] + orientationAngles[1] + orientationAngles[2])
+            )
+
             //if (event?.sensor == magnetoSensor){
             //    Log.d("")
             //}
         }
+    }
+
+    fun updateOrientationAngles() {
+        SensorManager.getRotationMatrix(
+            rotationMatrix,
+            null,
+            accelerometerRading,
+            magnetometerReading
+        )
+        SensorManager.getOrientation(rotationMatrix, orientationAngles)
+    }
+
+    private fun readAccSensorData(event: SensorEvent) {
+        System.arraycopy(event.values, 0, rawAccData, rawAccDataIndex, 3)
+        filterAccData()
+        binding.accelFiltered.text = getString(
+            R.string.accelerometer_filtered_template,
+            (filtAccData[0] + filtAccData[1] + filtAccData[2])
+        )
+    }
+
+    private fun readGyroSensorData(event: SensorEvent) {
+        System.arraycopy(event.values, 0, rawGyroData, rawGyroDataIndex, 3)
+        filterGyroData()
+        binding.gyroFiltered.text = getString(
+            R.string.gyroscope_filtered_template,
+            (filtGyroData[0] + filtGyroData[1] + filtGyroData[2])
+        )
+    }
+
+    private fun filterGyroData() {
+        val tm = System.nanoTime()
+        Log.d("tid", (tm / 1000000000.0f).toString())
+        val dt = ((tm - beginTime) / 1000000000.0f) / count
+        val alpha = rc / (rc + dt)
+        val isStarted = true
+
+        if (count == 0) {
+            filtGyroPrevData[0] = (1 - alpha) * rawGyroData[0]
+            filtGyroPrevData[1] = (1 - alpha) * rawGyroData[1]
+            filtGyroPrevData[2] = (1 - alpha) * rawGyroData[2]
+        } else {
+            filtGyroPrevData[0] = alpha * filtGyroPrevData[0] + (1 - alpha) * rawGyroData[0]
+            filtGyroPrevData[1] = alpha * filtGyroPrevData[1] + (1 - alpha) * rawGyroData[1]
+            filtGyroPrevData[2] = alpha * filtGyroPrevData[2] + (1 - alpha) * rawGyroData[2]
+        }
+        if (isStarted) {
+            filtGyroData[0] = filtGyroPrevData[0]
+            filtGyroData[1] = filtGyroPrevData[1]
+            filtGyroData[2] = filtGyroPrevData[2]
+
+        }
+    }
+
+    private fun filterAccData() {
+        val tm = System.nanoTime()
+        val dt = ((tm - beginTime) / 1000000000.0f) / count
+        val alpha = rc / (rc + dt)
+        val isStarted = true
+
+        if (count == 0) {
+            filtAccPrevData[0] = (1 - alpha) * rawAccData[0]
+            filtAccPrevData[1] = (1 - alpha) * rawAccData[1]
+            filtAccPrevData[2] = (1 - alpha) * rawAccData[2]
+        } else {
+            filtAccPrevData[0] = alpha * filtAccPrevData[0] + (1 - alpha) * rawAccData[0]
+            filtAccPrevData[1] = alpha * filtAccPrevData[1] + (1 - alpha) * rawAccData[1]
+            filtAccPrevData[2] = alpha * filtAccPrevData[2] + (1 - alpha) * rawAccData[2]
+        }
+        if (isStarted) {
+            filtAccData[0] = filtAccPrevData[0]
+            filtAccData[1] = filtAccPrevData[1]
+            filtAccData[2] = filtAccPrevData[2]
+        }
+        ++count
     }
 
 
