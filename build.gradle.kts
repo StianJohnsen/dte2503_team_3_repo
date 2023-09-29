633a373e0c5f45d688fa3cc7870f0bc8d505c924
yevhenverkhalantsev <97880997+yevhenverkhalantsev@users.noreply.github.com>
2023-09-29 18:26:11 +0200

    design to settings layout

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,9 @@ buildscript{
     }
 
     dependencies{
-        classpath("androidx.navigation.safeargs:androidx.navigation.safeargs.gradle.plugin:2.7.2")
+        classpath("androidx.navigation.safeargs:androidx.navigation.safeargs.gradle.plugin:2.7.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
-        classpath("com.google.gms:google-services:4.3.15")
+        classpath("com.google.gms:google-services:4.4.0")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.47")
     }
 }
@@ -21,7 +21,7 @@ buildscript{
 
 plugins {
     id("com.google.dagger.hilt.android") version "2.47" apply false
-    id("com.android.application") version "8.1.1" apply false
-    id("com.android.library") version "8.1.1" apply false
+    id("com.android.application") version "8.1.2" apply false
+    id("com.android.library") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
