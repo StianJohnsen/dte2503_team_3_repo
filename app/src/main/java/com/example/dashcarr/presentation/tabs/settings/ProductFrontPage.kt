db7de6fd1f107e4332c93f86174443f8a7469901
StianJohnsen <stianjohnzz1@gmail.com>
2023-10-11 19:43:59 +0100

    Scrollable info view about the new features. The view is visible when going into preferences in settings fragment

---
+++ app/src/main/java/com/example/dashcarr/presentation/tabs/settings/ProductFrontPage.kt
@@ -0,0 +1,47 @@
+package com.example.dashcarr.presentation.tabs.settings
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.dashcarr.R
+import com.example.dashcarr.databinding.FragmentProductFrontPageBinding
+import com.example.dashcarr.presentation.core.BaseFragment
+
+
+class ProductFrontPage : BaseFragment<FragmentProductFrontPageBinding>(
+    FragmentProductFrontPageBinding::inflate,
+    showBottomNavBar = false
+) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val adapter = ProductFrontPageAdapter()
+        binding.apply {
+            lifecycleOwner = viewLifecycleOwner
+            productInfoRecycler.adapter = adapter
+        }
+
+        val productInfoList = listOf<ProductInfo>(
+            ProductInfo(1,"MapView updates when your location changes\n\n"),
+            ProductInfo(2,"OpenStreetMap is implemented as the map technology\n\n"),
+            ProductInfo(3,"We use sensordata to estimate your current driving status\n\n"),
+            ProductInfo(4,"Start a driving session where you save all sensor data about your drive and later analysis\n\n"),
+            ProductInfo(5,"During the driving session you can also see your current: distance, elevation, elapsed time and speed\n\n"),
+            ProductInfo(6,"We have also introduced an experimental feature where you use your phone's display as a heads-up-display\n\n")
+        )
+
+            adapter.submitList(productInfoList)
+
+
+    }
+
+    override fun observeViewModel() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initListeners() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/example/dashcarr/presentation/tabs/settings/ProductInfo.kt
@@ -0,0 +1,6 @@
+package com.example.dashcarr.presentation.tabs.settings
+
+data class ProductInfo(
+    val id: Int,
+    val info: String
+)
