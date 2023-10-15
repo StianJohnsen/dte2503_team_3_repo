a1dd6f27cac48e32fc17a3ee7a024a0d0212acef
StianJohnsen <stianjohnzz1@gmail.com>
2023-10-15 14:13:39 +0200

    The recording buttons works now. Might want to think about hiding the buttons when they're pressed.

---
+++ app/src/main/java/com/example/dashcarr/presentation/tabs/map/RecordingViewModel.kt
@@ -0,0 +1,88 @@
+package com.example.dashcarr.presentation.tabs.map
+
+import android.os.Handler
+import android.os.Looper
+import android.os.SystemClock
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class RecordingViewModel : ViewModel() {
+
+    private var _startTimeMillis = MutableStateFlow<Long>(0)
+
+
+
+    private var _resumedElapsedTimeMillis = MutableStateFlow<Long>(0)
+
+
+    private var _pauseDuration = MutableStateFlow<Long>(0)
+
+    private var _pausedElapsedTimeMillis = MutableStateFlow<Long>(0)
+
+
+    private var _elapsedTime = MutableStateFlow<String>("")
+    val elapsedTime: StateFlow<String>
+        get() = _elapsedTime
+
+    var handler = Handler(Looper.getMainLooper())
+    private val updateTimeRunnable = object : Runnable {
+        override fun run() {
+            updateElapsedTime()
+            handler.postDelayed(this, 1000)
+        }
+    }
+
+    fun startRecording() {
+        viewModelScope.launch {
+            _startTimeMillis.emit(SystemClock.elapsedRealtime())
+        }
+        handler.post(updateTimeRunnable)
+    }
+
+    fun pauseRecording() {
+        viewModelScope.launch {
+            _pausedElapsedTimeMillis.emit(SystemClock.elapsedRealtime())
+        }
+        handler.removeCallbacks(updateTimeRunnable)
+    }
+
+    fun resumeRecording() {
+        viewModelScope.launch {
+            val currentTime = SystemClock.elapsedRealtime()
+            val timePaused = currentTime - _pausedElapsedTimeMillis.value
+            _pauseDuration.value += timePaused
+
+            _resumedElapsedTimeMillis.emit(currentTime)
+        }
+
+        handler.post(updateTimeRunnable)
+    }
+
+    fun stopRecording(){
+        handler.removeCallbacks(updateTimeRunnable)
+        viewModelScope.launch {
+            _startTimeMillis.emit(0)
+            _pausedElapsedTimeMillis.emit(0)
+            _pauseDuration.emit(0)
+            _resumedElapsedTimeMillis.emit(0)
+        }
+    }
+
+
+    fun updateElapsedTime() {
+
+        val currentTimeMillis = SystemClock.elapsedRealtime()
+        val updatedTime = (currentTimeMillis - _startTimeMillis.value) - _pauseDuration.value
+
+        val seconds = (updatedTime / 1000 % 60).toInt()
+        val minutes = (updatedTime / (1000 * 60) % 60).toInt()
+        val hours = (updatedTime / (1000 * 60 * 60) % 24).toInt()
+
+        viewModelScope.launch {
+            _elapsedTime.emit(String.format("%02d:%02d:%02d", hours, minutes, seconds))
+        }
+    }
+}
