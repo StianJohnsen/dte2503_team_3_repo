3b120062eeed85a4cbca50973e9907b3068a32bf
StianJohnsen <stianjohnzz1@gmail.com>
2023-10-16 17:36:51 +0200

    Fixed the visiblity of the recording. Removed unnused fragments. Removed new recording button in settings.

--- app/src/main/java/com/example/dashcarr/presentation/tabs/map/data/SensorData.kt
+++ app/src/main/java/com/example/dashcarr/presentation/tabs/map/data/SensorData.kt
@@ -1,4 +1,4 @@
-package com.example.dashcarr.presentation.tabs.settings
+package com.example.dashcarr.presentation.tabs.map.data
 
 data class SensorData(
     val timestamp: Long, val x: Float, val y: Float, val z: Float
--- app/src/main/java/com/example/dashcarr/presentation/tabs/settings/ConfigureRecordingViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.dashcarr.presentation.tabs.settings
-
-import androidx.lifecycle.ViewModel
-
-class ConfigureRecordingViewModel : ViewModel() {
-
-
-}
