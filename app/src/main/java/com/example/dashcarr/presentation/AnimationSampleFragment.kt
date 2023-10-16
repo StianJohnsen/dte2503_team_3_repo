c660d4437f497d339aa29915776606e3595b2ea9
Yves-Simon Zeulner <ysz@web.de>
2023-10-16 22:27:12 +0200

    Adding animation to "about us"

--- app/src/main/java/com/example/dashcarr/presentation/AnimationSampleFragment.kt
+++ app/src/main/java/com/example/dashcarr/presentation/AnimationSampleFragment.kt
@@ -2,8 +2,6 @@ package com.example.dashcarr.presentation
 
 import android.os.Bundle
 import android.view.View
-import androidx.navigation.fragment.findNavController
-import com.example.dashcarr.R
 import com.example.dashcarr.databinding.FragmentAnimationSampleBinding
 import com.example.dashcarr.presentation.core.BaseFragment
 
@@ -19,7 +17,7 @@ class AnimationSampleFragment : BaseFragment<FragmentAnimationSampleBinding>(
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.backToSettings.setOnClickListener {
-            findNavController().navigate(R.id.action_animationSampleFragment_to_action_settings)
+            requireActivity().onBackPressed()
         }
     }
 
