92a9c1d8a100c97990d840f2216aa5a5bd8aeb98
StianJohnsen <stianjohnzz1@gmail.com>
2023-11-22 08:40:30 +0100

    Rotating the device starts a new recording. This is unwanted behaviour. Will fix it later.

--- app/src/main/java/com/example/dashcarr/presentation/tabs/map/RecordingViewModel.kt
+++ app/src/main/java/com/example/dashcarr/presentation/tabs/map/RecordingViewModel.kt
@@ -1,88 +1,67 @@
 package com.example.dashcarr.presentation.tabs.map
 
-import android.os.Handler
-import android.os.Looper
 import android.os.SystemClock
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.asStateFlow
 
-class RecordingViewModel : ViewModel() {
+class RecordingViewModel {
 
     private var _startTimeMillis = MutableStateFlow<Long>(0)
-
     private var _resumedElapsedTimeMillis = MutableStateFlow<Long>(0)
-
-
     private var _pauseDuration = MutableStateFlow<Long>(0)
-
     private var _pausedElapsedTimeMillis = MutableStateFlow<Long>(0)
-
-
     private var _elapsedTime = MutableStateFlow<String>("")
-    val elapsedTime: StateFlow<String>
-        get() = _elapsedTime
+    val elapsedTime = _elapsedTime.asStateFlow()
 
-    var handler = Handler(Looper.getMainLooper())
-    private val updateTimeRunnable = object : Runnable {
-        override fun run() {
-            updateElapsedTime()
-            handler.postDelayed(this, 1000)
-        }
+    private var isRecording = false
+
+    suspend fun startRecording() {
+        _startTimeMillis.emit(SystemClock.elapsedRealtime())
+        isRecording = true
+        updateElapsedTime()
     }
 
-    fun startRecording() {
-        viewModelScope.launch {
-            _startTimeMillis.emit(SystemClock.elapsedRealtime())
-        }
-        handler.post(updateTimeRunnable)
-    }
-
-    fun pauseRecording() {
-        viewModelScope.launch {
+    suspend fun pauseRecording() {
+        if (isRecording) {
             _pausedElapsedTimeMillis.emit(SystemClock.elapsedRealtime())
+            isRecording = false
         }
-        handler.removeCallbacks(updateTimeRunnable)
     }
 
-    fun resumeRecording() {
-        viewModelScope.launch {
+    suspend fun resumeRecording() {
+        if (!isRecording) {
             val currentTime = SystemClock.elapsedRealtime()
             val timePaused = currentTime - _pausedElapsedTimeMillis.value
             _pauseDuration.value += timePaused
-
             _resumedElapsedTimeMillis.emit(currentTime)
+            isRecording = true
+            updateElapsedTime()
         }
-
-        handler.post(updateTimeRunnable)
     }
 
-    fun stopRecording() {
-        handler.removeCallbacks(updateTimeRunnable)
+    suspend fun stopRecording() {
+        isRecording = false
         // Reset all timing variables
-        viewModelScope.launch {
-            _startTimeMillis.emit(0)
-            _pausedElapsedTimeMillis.emit(0)
-            _pauseDuration.emit(0)
-            _resumedElapsedTimeMillis.emit(0)
-        }
+        _startTimeMillis.emit(0)
+        _pausedElapsedTimeMillis.emit(0)
+        _pauseDuration.emit(0)
+        _resumedElapsedTimeMillis.emit(0)
     }
 
+    private suspend fun updateElapsedTime() {
+        while (isRecording) {
+            val currentTimeMillis = SystemClock.elapsedRealtime()
+            // Corrects for pausing
+            val updatedTime = (currentTimeMillis - _startTimeMillis.value) - _pauseDuration.value
 
-    fun updateElapsedTime() {
+            val seconds = (updatedTime / 1000 % 60).toInt()
+            val minutes = (updatedTime / (1000 * 60) % 60).toInt()
+            val hours = (updatedTime / (1000 * 60 * 60) % 24).toInt()
 
-        val currentTimeMillis = SystemClock.elapsedRealtime()
-        // Corrects for pausing
-        val updatedTime = (currentTimeMillis - _startTimeMillis.value) - _pauseDuration.value
-
-        val seconds = (updatedTime / 1000 % 60).toInt()
-        val minutes = (updatedTime / (1000 * 60) % 60).toInt()
-        val hours = (updatedTime / (1000 * 60 * 60) % 24).toInt()
-
-        viewModelScope.launch {
             _elapsedTime.emit(String.format("%02d:%02d:%02d", hours, minutes, seconds))
+
+            delay(1000) // Delay for 1 second
         }
     }
 }
