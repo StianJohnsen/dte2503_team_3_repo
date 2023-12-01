0888ce57d8802104279bdf083788a02086b2c7e1
StianJohnsen <stianjohnzz1@gmail.com>
2023-12-01 17:01:35 +0100

    Sending email now works. Sending buttons only shows when the contact you have chosen has email or phone number filled in. History button in settings fragment now doesn't navigate to the Select Contact fragment.

--- app/src/main/java/com/example/dashcarr/presentation/tabs/social/selectMessage/SelectMessageViewModel.kt
+++ app/src/main/java/com/example/dashcarr/presentation/tabs/social/selectMessage/SelectMessageViewModel.kt
@@ -1,10 +1,14 @@
 package com.example.dashcarr.presentation.tabs.social.selectMessage
 
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.dashcarr.data.database.AppDatabase
+import com.example.dashcarr.data.database.dao.MessagesDao
 import com.example.dashcarr.domain.entity.FriendsEntity
+import com.example.dashcarr.domain.entity.MessagesEntity
 import com.example.dashcarr.domain.entity.SentMessagesEntity
 import com.example.dashcarr.domain.repository.IFriendsRepository
 import com.example.dashcarr.domain.repository.IMessagesRepository
@@ -28,22 +32,49 @@ class SelectMessageViewModel @Inject constructor(
     private val sentMessagesRepository: ISentMessagesRepository
 ) : ViewModel() {
 
+    private var _messagesList = MutableLiveData<List<MessagesEntity>>()
+
     val messagesList = messagesRepository.getAllMessagesLiveData()
 
-    private var _contactsList = MutableLiveData<FriendsEntity>()
-    val contactsList: LiveData<FriendsEntity> = _contactsList
+    private var _currentContact = MutableLiveData<FriendsEntity>()
+    val currentContact: LiveData<FriendsEntity> = _currentContact
 
-    fun getContact(id: Int?) {
+    fun getAllMessages(context: Context) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val db = AppDatabase.getInstance(context)
+            _messagesList.postValue(db.MessagesDao().getAllMessages())
+        }
+    }
+
+    fun getContact(id: Int, context: Context) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val db = AppDatabase.getInstance(context)
+            _currentContact.postValue(db.FriendsDao().getContactById(id))
+        }
+    }
+
+    /*
+
+        fun getContact(id: Int?) {
         if (id == null) return
         viewModelScope.launch(Dispatchers.IO) {
             if (_contactsList.value != null) return@launch
             _contactsList.postValue(friendsRepository.getFriendById(id).value)
         }
     }
+     */
 
     fun insertIntoSentMessages(sentMessagesEntity: SentMessagesEntity) {
         viewModelScope.launch(Dispatchers.IO) {
             sentMessagesRepository.saveNewSentMessage(sentMessagesEntity)
         }
+    }
+
+    fun insertIntoSentMessages(context: Context, sentMessagesEntity: SentMessagesEntity) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val db = AppDatabase.getInstance(context)
+            db.SentMessageDao().insert(sentMessagesEntity)
+        }
+
     }
 }
